aaaaa87cb077c87b361fa132699873e2df95bab3
Qowiyu Yusrizal <109859143+Exynos93@users.noreply.github.com>
2025-01-03 21:55:37 +0700

    Oracle.sol

--- Smart Contracts/Oracle.sol
+++ Smart Contracts/Oracle.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract Oracle {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor(address _priceFeed) {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+    }
+
+    function getLatestPrice() public view returns (int) {
+        (, int price, , , ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    // Additional methods for different asset types or risk assessments
+}
