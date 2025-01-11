b64fdae425371c5613b33c6d6f0d5ec07e6f998a
Qowiyu Yusrizal <109859143+Exynos93@users.noreply.github.com>
2025-01-11 10:28:46 -0500

    Oracle.sol

--- Smart Contracts/Oracle.sol
+++ Smart Contracts/Oracle.sol
@@ -1,19 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
 contract Oracle {
-    AggregatorV3Interface internal priceFeed;
+    uint256 public collateralRatio = 150; // Example: 150% collateral required
 
-    constructor(address _priceFeed) {
-        priceFeed = AggregatorV3Interface(_priceFeed);
+    function setCollateralRatio(uint256 _collateralRatio) public {
+        collateralRatio = _collateralRatio;
     }
 
-    function getLatestPrice() public view returns (int) {
-        (, int price, , , ) = priceFeed.latestRoundData();
-        return price;
+    function getCollateralRatio() public view returns (uint256) {
+        return collateralRatio;
     }
-
-    // Additional methods for different asset types or risk assessments
 }
